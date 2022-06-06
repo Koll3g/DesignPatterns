47bfbc2280933517fcbf9fbd40819ad99449c64e
Matthias Stieger <matthiass413@gmail.com>
2022-06-06 13:06:26 +0200

    Add abstract base class for shapes

---
+++ Prototype/Shape.cs
@@ -0,0 +1,21 @@
+﻿namespace Prototype
+{
+    public abstract class Shape : IShapePrototype
+    {
+        public int x;
+        public int y;
+
+        public Shape() {
+            x = 0;
+            y = 0;
+        }
+
+        public Shape(Shape source)
+        {
+            x = source.x;
+            y = source.y;
+        }
+
+        public abstract IShapePrototype clone();
+    }
+}
