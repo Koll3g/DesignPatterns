f63a2bdada40d94cf6e955f8cdf885d1c0ade4cc
leonieb1 <me.leoniebuser.ch@zbw-online.ch>
2022-06-06 11:34:05 +0200

    State Pattern inkl. Test

---
+++ State/Program.cs
@@ -0,0 +1,13 @@
+﻿namespace State
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // The client code.
+            var context = new Context(new ConcreteStateA());
+            context.Request1();
+            context.Request2();
+        }
+    }
+}
