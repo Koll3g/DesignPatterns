6359768ced4c690088b2fd5ed16b36090b7ef6c3
Matthias Stieger <matthiass413@gmail.com>
2022-06-06 13:07:15 +0200

    Add concrete classes cricle and rectangle

---
+++ Prototype/Circle.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Circle : Shape
+    {
+        public int radius;
+
+        public Circle(Circle source) : base(source) {
+            radius = source.radius;
+        }
+
+        public override IShapePrototype clone()
+        {
+            return new Circle(this);
+        }
+    }
+}
---
+++ Prototype/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Prototype
+{
+    public class Rectangle : Shape
+    {
+        public int width;
+        public int height;
+
+        public Rectangle(Rectangle source) : base(source) {
+            width = source.width;
+            height = source.height;
+        }
+
+        public override IShapePrototype clone()
+        {
+            return new Rectangle(this);
+        }
+    }
+}
