ec1dab0f0fb575593672538b34e5669d690cd9e0
faesslerm <markus.mfs@gmail.com>
2022-06-11 07:54:18 +0200

    Added proxy pattern

---
+++ Proxy/Service.cs
@@ -0,0 +1,16 @@
+﻿namespace Proxy
+{
+    public class Service : IService
+    {
+        private readonly IConsole _console;
+
+        public Service(IConsole console)
+        {
+            _console = console;
+        }
+        public void Operation(string user)
+        {
+            _console.WriteLine($"{user}: Operation called!");
+        }
+    }
+}
