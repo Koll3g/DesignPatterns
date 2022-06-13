d80add6a7a1eb2f26d376e6e6b349bd9609a1446
shintog <78559966+shintog@users.noreply.github.com>
2022-06-13 19:10:15 +0200

    Add TemplateMethod

---
+++ TemplateMethod/TemplateMethodPattern.cs
@@ -0,0 +1,29 @@
+﻿namespace tempalteMethod
+{
+    public abstract class AbstrakteKlasse
+    {
+        public abstract int PrimitiveOperation1();
+        public abstract int PrimitiveOperation2();
+
+        public void TemplateMethod()
+        {
+            int i = 1;
+            i -= PrimitiveOperation1();
+            i += PrimitiveOperation2();
+            Console.WriteLine(i);
+        }
+
+    }
+    public class KonkreteKlasse : AbstrakteKlasse
+    {
+        public override int PrimitiveOperation1()
+        {
+            return (DateTime.Today.DayOfWeek == DayOfWeek.Tuesday ? 1 : 2);
+        }
+
+        public override int PrimitiveOperation2()
+        {
+            return (DateTime.Today.Day >= 15 ? 1 : 2);
+        }
+    }
+}
