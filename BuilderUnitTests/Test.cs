c5e08117e8e28f2e528638adc94a90ea82a1d47b
Markus Staub <markusstaub1@gmail.com>
2022-06-13 21:50:23 +0200

    Build-Pattern implementiert inklusive Tests

---
+++ BuilderUnitTests/Test.cs
@@ -0,0 +1,33 @@
+﻿using Builder;
+using FluentAssertions;
+using Xunit;
+
+
+
+namespace BuilderTests
+{
+    public class Test
+    {
+        [Fact]
+        public void WhenBuildMinimalProduct_ThenProduct_ContainPartA1()
+        {
+            //Arrange
+            var director = new Director();
+            var builder = new ConcreteBuilder();
+
+
+            director.Builder = builder;
+
+            //Act
+            director.BuildMinimalViableProduct();
+            var result = builder.GetProduct().ListParts();
+
+            //result
+            result.Should().Contain("PartA1", result);
+
+        }
+
+
+
+    }
+}
