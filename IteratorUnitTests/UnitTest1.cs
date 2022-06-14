85c1311f9440a4a025a7ef82d4e77d7f052b4f8b
Milad Fakiry <m.fakiry3@gmail.com>
2022-06-14 15:57:50 +0200

    Started to write a test but not finished yet

--- IteratorUnitTests/UnitTest1.cs
+++ IteratorUnitTests/UnitTest1.cs
@@ -1,11 +1,25 @@
+using FluentAssertions;
+using Iterator;
+
 namespace IteratorUnitTests
 {
     public class UnitTest1
     {
         [Fact]
-        public void Test1()
+        public void CreateCollection_ItereateOverCollection_ResultTrueIfIteratingEnds()
         {
+            // arrange
+            ConcreteAggregate a = new ConcreteAggregate();
+            a[0] = "Item A";
+            a[1] = "Item B";
+            a[2] = "Item C";
+            a[3] = "Item D";
 
+            // act
+            var iterator = a.CreateIterator();
+
+            // assert
+            iterator.Should().Be("Iterating over collection");
         }
     }
 }
